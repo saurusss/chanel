24f308cb0c93907ee20dc5ca8bda019cfac4d2ad
saurusss <koc@kofficecom.co.kr>
2020-01-30 16:40:31 +0900

    매장리스트에서 버튼 양식 변경

--- app/models/itasset.rb
+++ app/models/itasset.rb
@@ -5,5 +5,14 @@ class Itasset < ApplicationRecord
   validates :store_id, :presence => true
   validates :mgmtno, presence: true, uniqueness: true
   validates_numericality_of :store_id
+  validates_format_of :ipaddr,  :with => /\A(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\z/,
+    :message => "Only IP address format allowed", :allow_blank => true 
+  validates_format_of :gwaddr, :with => /\A(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\z/,
+    :message => "Only IP address format allowed", :allow_blank => true 
+  validates_format_of :macwire, :with => /\A([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})\z/,
+    :message => "Only IP address format allowed", :allow_blank => true 
+  validates_format_of :macwireless, :with => /\A([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})\z/,
+    :message => "Only IP address format allowed", :allow_blank => true 
+
 
 end
